c3d1829f6e47b515cb39422dd3200532516d10a8
robinjoon <robin980108@naver.com>
2025-12-31 21:57:57 +0900

    회원 기능 구현 Phase 7: cors 예외 설정 코드 작성

--- server/src/main/kotlin/xyz/robinjoon/growweek/common/config/SecurityConfig.kt
+++ server/src/main/kotlin/xyz/robinjoon/growweek/common/config/SecurityConfig.kt
@@ -61,7 +61,7 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedOrigins = listOf("http://localhost:3000", "https://dev.growweek.robinjoon.xyz")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
